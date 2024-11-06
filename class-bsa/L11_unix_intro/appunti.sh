f94e6730862e0563d549e260709d3eaef3a2c83a
Alepiana13 <alessandro.piana01@universitadipavia.it>
2024-11-06 16:59:19 +0000

    appunti

---
+++ class-bsa/L11_unix_intro/appunti.sh
@@ -0,0 +1,19 @@
+cd sta per change directory
+.. serve per andare nella cartella prima
+- sta indicando la cartella precedente da mettere dopo cd
+ls elenca cosa ho nella cartella
+ls -l ci da informazioni sul file
+mkdir serve per creare una cartella, make directory è l'acronimo
+-wr vuol dire che l'utente puo scrivere e leggere, con la creazione di una nuova cartella ottenmgo dwr, posso fare tutto
+-F aggiunge uno slash che mi permette di vedere che posso avere potenzialmente qualcosa sotto quella cartella che ho appena creato
+mkdir -p  se la cartella esiste non da errore, se non esiste la crea e se gli diamo un intero percorso da fare lui le crea
+touch permette di creare un file
+copy la sintassi è copy sorgente origine
+rm remove serve per cancellare un file
+rm sottosot-r serve per eliminare 
+mv per muovere
+tail -n +4 file.txt | head -n 
+le variabili sono i nomi che io do a un valore
+echo
+\n identifica "a capo"
+per assegnare una variabile metto il segno uguale, quando la uso metto il dollaro e il ome assocuato a aquella variabile
